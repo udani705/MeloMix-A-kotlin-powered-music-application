e9dc813c9184c3ced6e9470f0ee2d43865976fc4
udani705 <udanibuddhima705@gmail.com>
2024-04-25 02:05:32 +0530

    Initial Commit

---
+++ app/src/main/java/com/group3/melomix/AboutActivity.kt
@@ -0,0 +1,23 @@
+package com.group3.melomix
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.group3.melomix.databinding.ActivityAboutBinding
+
+class AboutActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityAboutBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(MainActivity.currentThemeNav[MainActivity.themeIndex])
+        binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.title = "About"
+        binding.aboutText.text = aboutText()
+    }
+    private fun aboutText(): String{
+        return "Developed By: Group 03" +
+                "\n\nWe hope you're enjoying MeloMix"
+    }
+}
